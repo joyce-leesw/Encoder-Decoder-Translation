55983f419051717e5b915dd3a7ebc1d96dc5d68d
Joyce Shu <shuwei1203@gmail.com>
2023-03-19 00:27:20 +0000

    training first version of language model

---
+++ lib.py
@@ -0,0 +1,19 @@
+import string
+from keras.preprocessing.text import Tokenizer
+
+# remove all the string punctuations
+def clean_sentence(sentence):
+    lower_case = sentence.lower()
+    string_punctuation = string.punctuation + "¡" + '¿'    
+    clean_sentence = lower_case.translate(str.maketrans('', '', string_punctuation))
+    return clean_sentence
+
+# def clean_chi_sentence(sentence):
+#     string_punctuation = string.punctuation + "。" + "！" + "，" +"？"
+#     clean_sentence = sentence.translate(str.maketrans('', '', string_punctuation))
+#     return clean_sentence
+
+def tokenise(sentence):
+    txt_tokeniser = Tokenizer()
+    txt_tokeniser.fit_on_texts(sentence)
+    return txt_tokeniser.texts_to_sequences(sentence), txt_tokeniser
